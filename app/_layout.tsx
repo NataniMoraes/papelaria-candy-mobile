99993175c3b38b39fbd953e4ee4afa0d1b434d69
NataniMoraes <natanigabriele@gmail.com>
2025-11-11 21:37:22 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import React from 'react';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    // O <Stack> é o navegador de "pilha" que controla as telas
+    <Stack>
+      
+      <Stack.Screen 
+        name="(tabs)" 
+        options={{ headerShown: false }} // Esconde o cabeçalho duplicado
+      />
+
+      <Stack.Screen 
+        name="cadastro" // Refere-se ao arquivo app/cadastro.tsx
+        options={{
+          title: 'Cadastrar Novo Produto',
+        }}
+      />
+
+      <Stack.Screen 
+        name="detalhes" // Refere-se ao arquivo app/detalhes.tsx
+        options={{
+          title: 'Editar Produto', 
+          headerBackTitle: 'Voltar',
+        }}
+      />
+
+    </Stack>
   );
 }
